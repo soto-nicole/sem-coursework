3395e89c4888c1171613ec1f8951f64802684a5e
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-06 00:59:37 +0000

    Add ByRegion method to filter the city list by region

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -12,6 +12,7 @@ public class App
     {
         App app = new App();
         DatabaseUtil.connect();
+        //TODO: [NS] Maybe we should add all the variables here at the top and choose a certain region for all, country for all etc....
 
         //--------------------- All countries by population in descending order --------------------------------
         //1. All countries in the world sorted by population number in descending order (largest-to-smallest)
@@ -33,6 +34,10 @@ public class App
         //4. All cities in the world sorted by population number in descending order (largest-to-smallest)
         System.out.println("All cities in the world organised by largest population to smallest:");
         app.displayCities(AllCities.ByWorld());
+
+        //5. All cities in a region by population number in descending order (largest-to-smallest)
+        System.out.println("All cities in the " + region + " region organised by largest population to smallest:");
+        app.displayCities(AllCities.ByRegion(region));
 
         DatabaseUtil.disconnect();
     }
