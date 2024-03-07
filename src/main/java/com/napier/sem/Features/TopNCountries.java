1e9cf035a8b3d0cce0828d69b6a840ef6f4e816e
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-07 13:20:03 +0000

    Refactor

--- src/main/java/com/napier/sem/Features/TopNCountries.java
+++ src/main/java/com/napier/sem/Features/TopNCountries.java
@@ -1,22 +1,20 @@
 package com.napier.sem.Features;
 
+import com.napier.sem.Helpers.ReportHelper;
 import com.napier.sem.Models.Country;
-import com.napier.sem.Utils.DatabaseUtil;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class TopNCountries
 {
     /**
      * Gets a list of the top N populated countries in the world by their population number in descending order
+     *
      * @param N The number of top populated countries to be returned
      * @return ArrayList that contains Country objects in the world with their respective properties : code, name, continent, region, population and capital name
      */
     public static ArrayList<Country> ByWorld(int N)
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "ORDER BY country.Population DESC " +
@@ -25,13 +23,14 @@ public class TopNCountries
 
     /**
      * Gets a list of the top N populated countries in a given continent by their population number in descending order
-     * @param N The number of top populated countries to be returned
+     *
+     * @param N         The number of top populated countries to be returned
      * @param continent The name of the continent, constraining the list of countries returned to those within the specified continent
      * @return ArrayList that contains Country objects in a specific continent with their respective properties : code, name, continent, region, population and capital name
      */
     public static ArrayList<Country> ByContinent(int N, String continent)
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "WHERE country.Continent = '" + continent + "' " +
@@ -41,52 +40,18 @@ public class TopNCountries
 
     /**
      * Gets a list of the top N populated countries in a given region by their population number in descending order
-     * @param N The number of top populated countries to be returned
+     *
+     * @param N      The number of top populated countries to be returned
      * @param region The name of the region, constraining the list of countries returned to those within the specified region
      * @return ArrayList that contains Country objects in a specific region with their respective properties : code, name, continent, region, population and capital name
      */
     public static ArrayList<Country> ByRegion(int N, String region)
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "WHERE country.Region = '" + region + "' " +
                 "ORDER BY country.Population DESC " +
                 "LIMIT " + N);
-    }
-
-    /**
-     * Helper method to get the country information by using the SQL queries
-     * @param strSelect SQL query that will return the country information needed for the reports
-     * @return ArrayList from country objects that contains the required data being fetched
-     */
-    private static ArrayList<Country> getCountryReport(String strSelect)
-    {
-        ArrayList<Country> countries = new ArrayList<>();
-        try
-        {
-            Connection con = DatabaseUtil.getConnection();
-            Statement stmt = con.createStatement();
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            while (rset.next())
-            {
-                Country country = new Country();
-                country.code = rset.getString("Code");
-                country.name = rset.getString("Name");
-                country.continent = rset.getString("Continent");
-                country.region = rset.getString("Region");
-                country.population = rset.getInt("Population");
-                country.capitalName = rset.getString("CapitalName");
-                countries.add(country);
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get country report");
-            return null;
-        }
-        return countries;
     }
 }
