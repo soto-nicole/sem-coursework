aa70c993d91b6d43772407d1dbefbd906d29d734
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-07 13:20:29 +0000

    Add city district filter

--- src/main/java/com/napier/sem/Features/AllCountries.java
+++ src/main/java/com/napier/sem/Features/AllCountries.java
@@ -1,5 +1,6 @@
 package com.napier.sem.Features;
 
+import com.napier.sem.Helpers.ReportHelper;
 import com.napier.sem.Models.Country;
 import com.napier.sem.Utils.DatabaseUtil;
 import java.sql.Connection;
@@ -15,7 +16,7 @@ public class AllCountries
      */
     public static ArrayList<Country> ByWorld()
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "ORDER BY country.Population DESC");
@@ -28,7 +29,7 @@ public class AllCountries
      */
     public static ArrayList<Country> ByContinent(String continent)
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "WHERE country.Continent = '" + continent + "' " +
@@ -42,45 +43,10 @@ public class AllCountries
      */
     public static ArrayList<Country> ByRegion(String region)
     {
-        return getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
+        return ReportHelper.getCountryReport("SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as CapitalName " +
                 "FROM country " +
                 "JOIN city ON country.Capital = city.ID " +
                 "WHERE country.Region = '" + region + "' " +
                 "ORDER BY country.Population DESC");
-    }
-
-    /**
-     * Helper method to get the country information by using the SQL queries
-     * @param strSelect SQL query that will return the country information needed for the reports
-     * @return ArrayList from country objects that contains the required data being fetched
-     */
-    private static ArrayList<Country> getCountryReport(String strSelect)
-    {
-        ArrayList<Country> countries = new ArrayList<>();
-        try
-        {
-            Connection con = DatabaseUtil.getConnection();
-            Statement stmt = con.createStatement();
-            ResultSet rset = stmt.executeQuery(strSelect);
-
-            while (rset.next())
-            {
-                Country country = new Country();
-                country.code = rset.getString("Code");
-                country.name = rset.getString("Name");
-                country.continent = rset.getString("Continent");
-                country.region = rset.getString("Region");
-                country.population = rset.getInt("Population");
-                country.capitalName = rset.getString("CapitalName");
-                countries.add(country);
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get country report");
-            return null;
-        }
-        return countries;
     }
 }
