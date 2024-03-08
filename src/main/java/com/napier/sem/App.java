d2842072c5b8de5461086564e70d8f82136691f1
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2024-03-08 23:36:42 +0000

    Add call to TopNCities.ByDistrict() method in the app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -93,6 +93,11 @@ public class App
         System.out.println("Top " + N + " cities in the country " + country + " organised by largest population to smallest:");
         app.displayCities(TopNCities.ByCountry(N, country));
 
+        //16. Top N populated cities in a specific district sorted by population number in descending order (largest-to-smallest)
+
+        System.out.println("Top " + N + " cities in the district " + district + " organised by largest population to smallest:");
+        app.displayCities(TopNCities.ByDistrict(N, district));
+
         DatabaseUtil.disconnect();
     }
 
