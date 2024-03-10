5e6a1c186a699520987e8f087c27a79fe3bd1600
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-10 11:42:20 +0000

    Fix bug pipeline

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,6 +8,7 @@ import com.napier.sem.Models.Country;
 import com.napier.sem.Utils.DatabaseUtil;
 import com.napier.sem.View.Index;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,58 +16,110 @@ import java.util.Scanner;
 
 public class App
 {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         App app = new App();
         DatabaseUtil.connect();
 
-        while (true)
+        boolean continueLoop = true;
+        boolean isDefaultOption = false;
+
+        while (continueLoop)
         {
             Index.displayOptions();
             System.out.println("Select the number you wish to see the report for or press 0 to quit:");
-            String choice = scanner.nextLine();
 
-            if ("0".equals(choice)) break;
+            String choice = null;
+            long startTime = System.currentTimeMillis();
+            final long timeout = 20000; // 20 secs
 
-            Map<String, Runnable> keyValues = new HashMap<>();
-            String continent = "Africa";
-            String region = "Caribbean";
-            String country = "Spain";
-            String district = "Buenos Aires";
+            while (System.currentTimeMillis() - startTime < timeout && choice == null)
+            {
+                try
+                {
+                    if (System.in.available() > 0)
+                    {
+                        choice = scanner.nextLine();
+                        isDefaultOption = false;
+                    } else
+                    {
+                        Thread.sleep(200);
+                    }
+                }
+                catch (IOException | InterruptedException e)
+                {
+                    Thread.currentThread().interrupt();
+                    System.out.println("An error occurred. Exiting...");
+                    return;
+                }
+            }
 
-            //---------       All countries by population in descending order --------------
-            keyValues.put("1", () -> app.displayCountries(AllCountries.ByWorld()));
-            keyValues.put("2", () -> app.displayCountries(AllCountries.ByContinent(continent)));
-            keyValues.put("3", () -> app.displayCountries(AllCountries.ByRegion(region)));
+            // choosing default afterr 20 seconds
+            if (choice == null)
+            {
+                choice = "1";
+                isDefaultOption = true;
+            }
 
-            //---------       All cities by population in descending order    ---------------
-            keyValues.put("4", () -> app.displayCities(AllCities.ByWorld()));
-            keyValues.put("5", () -> app.displayCities(AllCities.ByContinent(continent)));
-            keyValues.put("6", () -> app.displayCities(AllCities.ByRegion(region)));
-            keyValues.put("7", () -> app.displayCities(AllCities.ByCountry(country)));
-            keyValues.put("8", () -> app.displayCities(AllCities.ByDistrict(district)));
+            if ("0".equals(choice))
+            {
+                continueLoop = false;
+            }
 
+            else
+            {
+                Runnable action = getUserOption(app, choice);
+                action.run();
 
-            //-------------- Top N countries by population in descending order --------------
-            keyValues.put("9", () -> app.displayCountries(TopNCountries.ByWorld(getN(scanner))));
-            keyValues.put("10", () -> app.displayCountries(TopNCountries.ByContinent(getN(scanner), continent)));
-            keyValues.put("11", () -> app.displayCountries(TopNCountries.ByRegion(getN(scanner), region)));
-
-            //---------------Top N cities by population in descending order -------------------
-            keyValues.put("12", () -> app.displayCities(TopNCities.ByWorld(getN(scanner))));
-            keyValues.put("13", () -> app.displayCities(TopNCities.ByContinent(getN(scanner), continent)));
-            keyValues.put("14", () -> app.displayCities(TopNCities.ByRegion(getN(scanner), region)));
-            keyValues.put("15", () -> app.displayCities(TopNCities.ByCountry(getN(scanner), country)));
-            keyValues.put("16", () -> app.displayCities(TopNCities.ByDistrict(getN(scanner), district)));
-
-            Runnable action = keyValues.getOrDefault(choice, () -> System.out.println("Invalid choice."));
-            action.run();
+                if (isDefaultOption)
+                {
+                    continueLoop = false; //exit the loop if the desfault value is used after 20 secs
+                }
+            }
         }
 
         DatabaseUtil.disconnect();
         scanner.close();
         System.out.println("Thank for using our system!");
     }
+    private static Runnable getUserOption(App app, String choice)
+    {
+        Map<String, Runnable> keyValues = new HashMap<>();
+        String continent = "Africa";
+        String region = "Caribbean";
+        String country = "Spain";
+        String district = "Buenos Aires";
+
+        //---------       All countries by population in descending order --------------
+        keyValues.put("1", () -> app.displayCountries(AllCountries.ByWorld()));
+        keyValues.put("2", () -> app.displayCountries(AllCountries.ByContinent(continent)));
+        keyValues.put("3", () -> app.displayCountries(AllCountries.ByRegion(region)));
+
+        //---------       All cities by population in descending order    ---------------
+        keyValues.put("4", () -> app.displayCities(AllCities.ByWorld()));
+        keyValues.put("5", () -> app.displayCities(AllCities.ByContinent(continent)));
+        keyValues.put("6", () -> app.displayCities(AllCities.ByRegion(region)));
+        keyValues.put("7", () -> app.displayCities(AllCities.ByCountry(country)));
+        keyValues.put("8", () -> app.displayCities(AllCities.ByDistrict(district)));
+
+
+        //-------------- Top N countries by population in descending order --------------
+        keyValues.put("9", () -> app.displayCountries(TopNCountries.ByWorld(getN(scanner))));
+        keyValues.put("10", () -> app.displayCountries(TopNCountries.ByContinent(getN(scanner), continent)));
+        keyValues.put("11", () -> app.displayCountries(TopNCountries.ByRegion(getN(scanner), region)));
+
+        //---------------Top N cities by population in descending order -------------------
+        keyValues.put("12", () -> app.displayCities(TopNCities.ByWorld(getN(scanner))));
+        keyValues.put("13", () -> app.displayCities(TopNCities.ByContinent(getN(scanner), continent)));
+        keyValues.put("14", () -> app.displayCities(TopNCities.ByRegion(getN(scanner), region)));
+        keyValues.put("15", () -> app.displayCities(TopNCities.ByCountry(getN(scanner), country)));
+        keyValues.put("16", () -> app.displayCities(TopNCities.ByDistrict(getN(scanner), district)));
+
+        return keyValues.getOrDefault(choice, () -> System.out.println("Invalid choice."));
+    }
+
+
+
 
     /**
      * Displays the countries' details in a formatted table
