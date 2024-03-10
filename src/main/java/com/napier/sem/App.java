1163c68fea18d4db5d0f856b442ee728d13fae75
CatVine <catriona.vine@neonwavestudios.co.uk>
2024-03-10 19:09:00 +0000

    Add AllCapitalCities feature

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,6 +3,7 @@ import com.napier.sem.Features.AllCities;
 import com.napier.sem.Features.AllCountries;
 import com.napier.sem.Features.TopNCountries;
 import com.napier.sem.Features.TopNCities;
+import com.napier.sem.Features.AllCapitalCities;
 import com.napier.sem.Models.City;
 import com.napier.sem.Models.Country;
 import com.napier.sem.Utils.DatabaseUtil;
@@ -118,9 +119,6 @@ public class App
         return keyValues.getOrDefault(choice, () -> System.out.println("Invalid choice."));
     }
 
-
-
-
     /**
      * Displays the countries' details in a formatted table
      * @param countries The list of Country objects that will be displayed
@@ -171,7 +169,29 @@ public class App
         System.out.println("`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````");
     }
 
-    private static int getN(Scanner scanner)
+    /**
+     * Displays the capital cities' details in a formatted table
+     * @param capitalCities The list of City objects that will be displayed
+     */
+    public void displayCapitalCities(ArrayList<City> capitalCities) {
+        if (capitalCities == null) {
+            System.out.println("No capital cities");
+            return;
+        }
+
+        System.out.println(String.format("%-35s %-60s %-15s", "Name", "Country", "Population"));
+
+        for (City capitalCity : capitalCities) {
+            if (capitalCity == null) continue;
+            String capitalCityString = String.format("%-35s %-60s %-15d",
+                    capitalCity.name, capitalCity.countryCode, capitalCity.population);
+            System.out.println(capitalCityString);
+        }
+
+        System.out.println("`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````");
+    }
+
+        private static int getN(Scanner scanner)
     {
         System.out.println("How many top N would you like to see?:");
         int N = scanner.nextInt();
---
+++ src/main/java/com/napier/sem/Features/AllCapitalCities.java
@@ -0,0 +1,20 @@
+package com.napier.sem.Features;
+
+import com.napier.sem.Helpers.ReportHelper;
+import com.napier.sem.Models.City;
+import java.util.ArrayList;
+
+public class AllCapitalCities {
+    /**
+     * Gets a list of all the capital cities in the world by their population number in descending order
+     *
+     * @return ArrayList that contains City objects in the world with their respective properties : Name, Country and Population
+     */
+    public static ArrayList<City> ByWorld() {
+        return ReportHelper.getCityReport("SELECT city.Name, country.Name as CountryName, city.Population " +
+                "FROM city " +
+                "JOIN country ON city.CountryCode = country.Code " +
+                "WHERE country.capital = city.id " +
+                "ORDER BY city.Population DESC");
+    }
+}
