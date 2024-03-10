d63304deba58e083fe56fc1a316df5b924634068
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2024-03-10 22:38:28 +0000

    Add TopNCapitalCities.ByWorld() Method

--- src/main/java/com/napier/sem/View/Index.java
+++ src/main/java/com/napier/sem/View/Index.java
@@ -32,5 +32,7 @@ public class Index
         System.out.println("  17)  List of capital cities in the world by population in descending order");
         System.out.println("  18)  List of capital cities in a continent by population in descending order");
         System.out.println("  19)  List of capital cities in a region by population in descending order");
+        System.out.println();
+        System.out.println("  20)  Top populated capital cities in the world in descending order");
     }
 }
