258501feff657fd1e43d238a490e17907bb7bb91
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-11 09:41:12 +0000

    Tidy up of App class

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class App
 {
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         App app = new App();
         DatabaseUtil.connect();
@@ -65,7 +66,7 @@ public class App
 
             else
             {
-                Runnable action = getUserOption(app, choice);
+                Runnable action = Index.getUserOption(app, choice, scanner);
                 action.run();
 
                 if (isDefaultOption)
@@ -79,50 +80,7 @@ public class App
         scanner.close();
         System.out.println("Thanks for using our system!");
     }
-    private static Runnable getUserOption(App app, String choice)
-    {
-        Map<String, Runnable> keyValues = new HashMap<>();
-        String continent = "Africa";
-        String region = "Caribbean";
-        String country = "Spain";
-        String district = "Buenos Aires";
 
-        //---------       All countries by population in descending order --------------
-        keyValues.put("1", () -> app.displayCountries(AllCountries.ByWorld()));
-        keyValues.put("2", () -> app.displayCountries(AllCountries.ByContinent(continent)));
-        keyValues.put("3", () -> app.displayCountries(AllCountries.ByRegion(region)));
-
-        //---------       All cities by population in descending order    ---------------
-        keyValues.put("4", () -> app.displayCities(AllCities.ByWorld()));
-        keyValues.put("5", () -> app.displayCities(AllCities.ByContinent(continent)));
-        keyValues.put("6", () -> app.displayCities(AllCities.ByRegion(region)));
-        keyValues.put("7", () -> app.displayCities(AllCities.ByCountry(country)));
-        keyValues.put("8", () -> app.displayCities(AllCities.ByDistrict(district)));
-
-        //-------------- Top N countries by population in descending order --------------
-        keyValues.put("9", () -> app.displayCountries(TopNCountries.ByWorld(getN(scanner))));
-        keyValues.put("10", () -> app.displayCountries(TopNCountries.ByContinent(getN(scanner), continent)));
-        keyValues.put("11", () -> app.displayCountries(TopNCountries.ByRegion(getN(scanner), region)));
-
-        //---------------Top N cities by population in descending order -------------------
-        keyValues.put("12", () -> app.displayCities(TopNCities.ByWorld(getN(scanner))));
-        keyValues.put("13", () -> app.displayCities(TopNCities.ByContinent(getN(scanner), continent)));
-        keyValues.put("14", () -> app.displayCities(TopNCities.ByRegion(getN(scanner), region)));
-        keyValues.put("15", () -> app.displayCities(TopNCities.ByCountry(getN(scanner), country)));
-        keyValues.put("16", () -> app.displayCities(TopNCities.ByDistrict(getN(scanner), district)));
-
-        //---------       All capital cities by population in descending order    ---------------
-        keyValues.put("17", () -> app.displayCapitalCities(AllCapitalCities.ByWorld()));
-        keyValues.put("18", () -> app.displayCapitalCities(AllCapitalCities.ByContinent(continent)));
-        keyValues.put("19", () -> app.displayCapitalCities(AllCapitalCities.ByRegion(region)));
-
-        //---------       Top N capital cities by population in descending order    ---------------
-        keyValues.put("20", () -> app.displayCapitalCities(TopNCapitalCities.ByWorld(getN(scanner))));
-        keyValues.put("21", () -> app.displayCapitalCities(TopNCapitalCities.ByContinent(getN(scanner), continent)));
-        keyValues.put("22", () -> app.displayCapitalCities(TopNCapitalCities.ByRegion(getN(scanner), region)));
-
-        return keyValues.getOrDefault(choice, () -> System.out.println("Invalid choice."));
-    }
 
     /**
      * Displays the countries' details in a formatted table
@@ -178,15 +136,18 @@ public class App
      * Displays the capital cities' details in a formatted table
      * @param capitalCities The list of City objects that will be displayed
      */
-    public void displayCapitalCities(ArrayList<City> capitalCities) {
-        if (capitalCities == null) {
+    public void displayCapitalCities(ArrayList<City> capitalCities)
+    {
+        if (capitalCities == null)
+        {
             System.out.println("No capital cities");
             return;
         }
 
         System.out.println(String.format("%-35s %-60s %-15s", "Name", "Country", "Population"));
 
-        for (City capitalCity : capitalCities) {
+        for (City capitalCity : capitalCities)
+        {
             if (capitalCity == null) continue;
             String capitalCityString = String.format("%-35s %-60s %-15d",
                     capitalCity.name, capitalCity.countryCode, capitalCity.population);
@@ -194,13 +155,5 @@ public class App
         }
 
         System.out.println("`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````");
-    }
-
-        private static int getN(Scanner scanner)
-    {
-        System.out.println("How many top N would you like to see?:");
-        int N = scanner.nextInt();
-        scanner.nextLine();
-        return N;
     }
 }
--- src/main/java/com/napier/sem/View/Index.java
+++ src/main/java/com/napier/sem/View/Index.java
@@ -1,5 +1,13 @@
 package com.napier.sem.View;
 
+import com.napier.sem.App;
+import com.napier.sem.Features.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 public class Index
 {
     public static void displayOptions()
@@ -36,5 +44,58 @@ public class Index
         System.out.println("  20)  Top populated capital cities in the world in descending order");
         System.out.println("  21)  Top populated capital cities in a continent in descending order");
         System.out.println("  22)  Top populated capital cities in a region in descending order");
+    }
+
+    public static Runnable getUserOption(App app, String choice, Scanner scanner)
+    {
+        Map<String, Runnable> keyValues = new HashMap<>();
+        String continent = "Africa";
+        String region = "Caribbean";
+        String country = "Spain";
+        String district = "Buenos Aires";
+
+        //---------       All countries by population in descending order --------------
+        keyValues.put("1", () -> app.displayCountries(AllCountries.ByWorld()));
+        keyValues.put("2", () -> app.displayCountries(AllCountries.ByContinent(continent)));
+        keyValues.put("3", () -> app.displayCountries(AllCountries.ByRegion(region)));
+
+        //---------       All cities by population in descending order    ---------------
+        keyValues.put("4", () -> app.displayCities(AllCities.ByWorld()));
+        keyValues.put("5", () -> app.displayCities(AllCities.ByContinent(continent)));
+        keyValues.put("6", () -> app.displayCities(AllCities.ByRegion(region)));
+        keyValues.put("7", () -> app.displayCities(AllCities.ByCountry(country)));
+        keyValues.put("8", () -> app.displayCities(AllCities.ByDistrict(district)));
+
+        //-------------- Top N countries by population in descending order --------------
+        keyValues.put("9", () -> app.displayCountries(TopNCountries.ByWorld(getN(scanner))));
+        keyValues.put("10", () -> app.displayCountries(TopNCountries.ByContinent(getN(scanner), continent)));
+        keyValues.put("11", () -> app.displayCountries(TopNCountries.ByRegion(getN(scanner), region)));
+
+        //---------------Top N cities by population in descending order -------------------
+        keyValues.put("12", () -> app.displayCities(TopNCities.ByWorld(getN(scanner))));
+        keyValues.put("13", () -> app.displayCities(TopNCities.ByContinent(getN(scanner), continent)));
+        keyValues.put("14", () -> app.displayCities(TopNCities.ByRegion(getN(scanner), region)));
+        keyValues.put("15", () -> app.displayCities(TopNCities.ByCountry(getN(scanner), country)));
+        keyValues.put("16", () -> app.displayCities(TopNCities.ByDistrict(getN(scanner), district)));
+
+        //---------       All capital cities by population in descending order    ---------------
+        keyValues.put("17", () -> app.displayCapitalCities(AllCapitalCities.ByWorld()));
+        keyValues.put("18", () -> app.displayCapitalCities(AllCapitalCities.ByContinent(continent)));
+        keyValues.put("19", () -> app.displayCapitalCities(AllCapitalCities.ByRegion(region)));
+
+        //---------       Top N capital cities by population in descending order    ---------------
+        keyValues.put("20", () -> app.displayCapitalCities(TopNCapitalCities.ByWorld(getN(scanner))));
+        keyValues.put("21", () -> app.displayCapitalCities(TopNCapitalCities.ByContinent(getN(scanner), continent)));
+        keyValues.put("22", () -> app.displayCapitalCities(TopNCapitalCities.ByRegion(getN(scanner), region)));
+
+        return keyValues.getOrDefault(choice, () -> System.out.println("Invalid choice."));
+    }
+
+    public static int getN(Scanner scanner)
+    {
+        System.out.println("How many top N would you like to see?:");
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        return N;
     }
 }
