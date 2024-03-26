1f5758d117150644e3b5e392ec42cf714648ab79
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2024-03-26 12:09:26 +0000

    Add Population Class

---
+++ src/main/java/com/napier/sem/Models/Population.java
@@ -0,0 +1,24 @@
+package com.napier.sem.Models;
+
+/**
+ * A class that creates Population objects
+ */
+public class Population
+{
+    /**
+     * The name of the area the population is being reported of
+     */
+    public String areaName;
+    /**
+     * The total number of people within the area
+     */
+    public int population;
+    /**
+     * The total number of people who live within cities
+     */
+    public int populationCities;
+    /**
+     * The total number of people who live outwith cities
+     */
+    public int populationOutsideCities;
+}
