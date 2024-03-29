1afe53200138d1ada7250a1bec91e81e7b55259e
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2024-03-29 19:15:54 +0000

    Add Language model

---
+++ src/main/java/com/napier/sem/Models/LanguageSpeakers.java
@@ -0,0 +1,16 @@
+package com.napier.sem.Models;
+
+public class LanguageSpeakers {
+    /**
+     * The name of the language the number of speakers is being reported for
+     */
+    public String languageName;
+    /**
+     * The total number of people in the world who speak this language
+     */
+    public long totalSpeakers;
+    /**
+     * The total number of people in the world who speak this language as a percentage of the world population
+     */
+    public float totalSpeakersPercentage;
+}
